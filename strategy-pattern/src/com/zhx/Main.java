2c436415029f3fe4cb484599de7385f10dc8bc23
xian <897679403@qq.com>
2018-06-26 11:07:11 +0800

    第一个练习完成

--- strategy-pattern/src/com/zhx/Main.java
+++ strategy-pattern/src/com/zhx/Main.java
@@ -1,7 +1,14 @@
 package com.zhx;
 
+import com.zhx.tools.AxeBehavior;
+import com.zhx.user.King;
+
 public class Main {
     public static void main(String[] args) {
-
+        King user1 = new King();
+        user1.fight();
+        user1.setWeapon(new AxeBehavior());
+        System.out.println("I got a weapon Axe!");
+        user1.fight();
     }
 }
--- strategy-pattern/src/com/zhx/user/Queen.java
+++ strategy-pattern/src/com/zhx/user/Queen.java
@@ -1,9 +1,13 @@
 package com.zhx.user;
 
-public class Queen implements Character{
+import com.zhx.tools.BowAndArrowBehavior;
 
-    @Override
-    public void fight() {
-        System.out.println("I am a Queen! Fight!");
+public class Queen extends Character{
+
+    public Queen(){
+        super(new BowAndArrowBehavior());
+        System.out.println("I am a Queen!");
+        System.out.println("I born with a BowAndArrow!");
     }
+
 }
